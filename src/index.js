ada6d8527811d1065f7649e0ffab496f1a597eed
sakata <hironori.sakata@smarthr.co.jp>
2023-08-14 12:11:08 +0900

    Part5まで

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import store from './app/store'
 import { Provider } from 'react-redux'
 
 import { worker } from './api/server'
+import { fetchUsers } from './features/users/usersSlice'
 
 // Wrap app rendering so we can wait for the mock API to initialize
-async function start() {
+async function main() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
+
+  store.dispatch(fetchUsers())
 
   ReactDOM.render(
     <React.StrictMode>
@@ -22,4 +25,4 @@ async function start() {
   )
 }
 
-start()
+main()
